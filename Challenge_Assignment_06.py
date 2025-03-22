25b1794c9b1fb7e317459349494c627a0ef429d9
riukui <riukui@naver.com>
2025-03-22 12:17:17 +0900

    Challenge_Assignment_06 edited

--- Challenge_Assignment_06.py
+++ Challenge_Assignment_06.py
@@ -1,5 +1,17 @@
 import requests
+import json
+
+api_url = "https://nomad-movies.nomadcoders.workers.dev/movies/"
 
 movie_ids = [
     238, 680, 550, 185, 641, 515042, 152532, 120467, 872585, 906126, 840430
 ]
+
+for movie_id in movie_ids:
+    url = f"{api_url}{movie_id}"
+    response = requests.get(url)
+    data = response.json()
+    print(f"title : {data['title']}")
+    print(f"overview : {data['overview']}")
+    print(f"vote_average : {data['vote_average']}")
+    print("---------------------------------")
